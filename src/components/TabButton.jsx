0eab01578c526a79369ebab6d60d08d6a5aa0499
Son, Eunhyeok <dmsgur12772@gmail.com>
2026-04-16 17:00:35 +0900

    리스트 데이터 동적 출력

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -2,7 +2,9 @@ export default function TabButton(props) {
     
     return (
     <li>
-        <button onClick={props.onSelect}>{props.children}</button>
+        <button className={props.isSelected ? 'active' : undefined} onClick={props.onSelect}>
+            {props.children}
+        </button>
     </li>
     );
 }
