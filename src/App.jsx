0ef502e9dfca8a91a9c73623e1fc2b7893c63398
Son, Eunhyeok <dmsgur12772@gmail.com>
2026-04-16 16:21:11 +0900

    State(상태) 관리 & Hooks(훅) 사용법

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import {useState} from 'react';
+
 import { CORE_CONCEPTS } from './data.js';
 import { Header } from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
+  const [ selectedTopic, setSelectedTopic ] = useState('Pleases click a button');
+
   function handleSelect(selectedButton) {
     // selectedButton => 'components', 'jsx', props', 'state'
-    console.log(selectedButton);
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
   }
 
   return (
@@ -38,7 +43,7 @@ function App() {
             <TabButton onSelect={() =>handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() =>handleSelect('state')}>State</TabButton>
           </menu>
-          
+          {selectedTopic}
         </section>
         <h2>Time to get started!</h2>
       </main>
