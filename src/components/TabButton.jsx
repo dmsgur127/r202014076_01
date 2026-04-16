ae994e02932cc9e930f1ccd8561e8dccabf06290
Son, Eunhyeok <dmsgur12772@gmail.com>
2026-04-16 15:50:01 +0900

    이벤트 처리

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,3 +1,14 @@
 export default function TabButton(props) {
-    return <li><button>{props.children}</button></li>
+    // document.querySelector('button').addEventListener('click', () => {})
+    // 자바 스크립트 방식
+
+    function handleClick() {
+        console.log('hello World!');
+    }
+
+    return (
+    <li>
+        <button onClick={handleClick}>{props.children}</button>
+    </li>
+    );
 }
