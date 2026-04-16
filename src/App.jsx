41fa8e76e5432ed3dc9f1e22e841b4af473b125b
Son, Eunhyeok <dmsgur12772@gmail.com>
2026-04-16 15:59:25 +0900

    함수를 속성의 값으로 전달하기

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
+  function handleSelect() {
+    console.log('hello World - selected!');
+  }
+
   return (
     <div>
       <Header/>
@@ -28,10 +32,10 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={handleSelect}>Components</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
           </menu>
         </section>
         <h2>Time to get started!</h2>
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,14 +1,8 @@
 export default function TabButton(props) {
-    // document.querySelector('button').addEventListener('click', () => {})
-    // 자바 스크립트 방식
-
-    function handleClick() {
-        console.log('hello World!');
-    }
-
+    
     return (
     <li>
-        <button onClick={handleClick}>{props.children}</button>
+        <button onClick={props.onSelect}>{props.children}</button>
     </li>
     );
 }
